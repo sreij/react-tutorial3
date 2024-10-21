f77bf74c6599e2514ec80bee900fb38064091444
sreij <chrj23020@g.nihon-u.ac.jp>
2024-10-21 10:08:06 +0900

    クリックイベントの反映

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 export default function App() {
     const [bright, setBright]=useState("darken");
+    const [full, fullImage] = useState("image/pic1.jpg");
+    /*
     const images = [
         "images/pic1.jpg",
         "images/pic2.jpg",
@@ -16,16 +18,14 @@ export default function App() {
         pic4: "Section of wall from a pharoah's tomb",
         pic5: "Large moth on a leaf"
     }
-    /*
-    const images=[
-        <img key="pic1" src="images/pic1.jpg" alt="Closeup of a human eye" />,
-        <img key="pic2" src="images/pic2.jpg" alt="Rock that looks like a wave" />,
-        <img key="pic3" src="images/pic3.jpg" alt="Purple and white pansies" />,
-        <img key="pic4" src="images/pic4.jpg" alt="Section of wall from a pharoah's tomb" />,
-        <img key="pic5" src="images/pic5.jpg" alt="Large moth on a leaf" />
-    ];
     */
-
+    const images=[
+        <img key="pic1" src="images/pic1.jpg" alt="Closeup of a human eye" onClick={() => fullImage("pic1")} />,
+        <img key="pic2" src="images/pic2.jpg" alt="Rock that looks like a wave" onClick={() => fullImage("pic2")} />,
+        <img key="pic3" src="images/pic3.jpg" alt="Purple and white pansies" onClick={() => fullImage("pic3")} />,
+        <img key="pic4" src="images/pic4.jpg" alt="Section of wall from a pharoah's tomb" onClick={() => fullImage("pic4")} />,
+        <img key="pic5" src="images/pic5.jpg" alt="Large moth on a leaf" onClick={() => fullImage("pic5")} />
+    ];
     function brightness(){
         if(bright === "darken"){
             setBright("lighten");
@@ -48,7 +48,7 @@ export default function App() {
           <button className="dark" onClick={brightness}>{bright}</button>
         </div>
         <div className="thumb-bar">
-            
+            {images}
         </div>
       </>
     );
